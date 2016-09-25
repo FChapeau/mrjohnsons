9ba9d3c7ff3c6430731cf291aac0ffcb9da3daaa
Unknown <ralph.dion@gmail.com>
2016-09-25 14:53:16 -0400

    Changed img location + corrected JS and CSS

--- public/js/rungenerator.js
+++ public/js/rungenerator.js
@@ -2,24 +2,27 @@ var runGenerator=
 {
     runComponents : {
         location: ["at a bar", "at a warehouse", "in the barren district", "in a matrix host", "in a moving vehicule", "in astral plane"],
-        employers: ["a secret society", "a political group", "a government agency", "a minor corporation", "a criminal syndicate", "a magical group", "a private individual", "an exotic being"],
+        employer: ["a secret society", "a political group", "a government agency", "a minor corporation", "a criminal syndicate", "a magical group", "a private individual", "an exotic being"],
         jobType: ["a data steal", "an assassination", "a demolition", "an extraction", "an insertion", "a misdirection", "a protection", "a delivery"],
-        macguffin: ["a key employee", "a prototype product", "cutting edge technology research", "a bioengineered life form", "a magical object", "an urban building", "a rural location"],
-        twists: ["security is unexpectedly high", "a third party is also interested", "the target is not what is appears to be", "the job requires a rare piece of equipment", "target has moved", "the employer decides to double-cross them"]
+        macguffin: ["a key employee", "a prototype product", "cutting edge technology research", "a bioengineered life form", "a magical object"],
+        twist: ["security is unexpectedly high", "a third party is also interested", "the target is not what is appears to be", "the job requires a rare piece of equipment", "target has moved", "the employer decides to double-cross them"]
     },
+
     generate:
         function(component){
             var randomElement = Math.floor(Math.random() * this.runComponents[component].length);
-            document.getElementById(component).innerHTML = this.runComponents[component][randomElement];
+            var cardId = component + "Card";
+              document.getElementById(component).innerHTML = this.runComponents[component][randomElement];
+              document.getElementById(cardId).innerHTML= this.runComponents[component][randomElement];
         },
     generateAll:
     function(){
         runGenerator.generate("location");
-        runGenerator.generate("employers");
+        runGenerator.generate("employer");
         runGenerator.generate("jobType");
         runGenerator.generate("macguffin");
-        runGenerator.generate("twists");
-//generateAll doesn't change card, dunno why
+        runGenerator.generate("twist");
+
     }
 };
 document.getElementById("genBtn").addEventListener("click", runGenerator.generateAll);
@@ -28,7 +31,7 @@ document.getElementById("chgLocation").addEventListener("click", function(){
     runGenerator.generate("location");
 });
 document.getElementById("chgEmployer").addEventListener("click", function(){
-    runGenerator.generate("employers");
+    runGenerator.generate("employer");
 });
 document.getElementById("chgJob").addEventListener("click", function(){
     runGenerator.generate("jobType");
@@ -37,7 +40,7 @@ document.getElementById("chgMcguffin").addEventListener("click", function(){
     runGenerator.generate("macguffin");
 });
 document.getElementById("chgTwist").addEventListener("click", function(){
-    runGenerator.generate("twists");
+    runGenerator.generate("twist");
 });
 
 runGenerator.generateAll();
